302ac7c4b61f66d53cf7124f510a984ca87682da
Kei Watanabe <kei_watanabe@brown.edu>
2023-11-23 19:27:10 -0500

    i should have never updated my mac to sonoma, it feels like im working with a uncooperative toddler

--- Lawn.java
+++ Lawn.java
@@ -1,20 +1,33 @@
 package indy;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.util.Duration;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class Lawn {
     private LawnSquare[][] lawnGraphic;
     private PeaShooter[][] plantBoard;
-    private LinkedList<NormalZombie>[] totalZombies;
+    private ArrayList<LinkedList<NormalZombie>> totalZombies;
 
     public Lawn(Pane gamepane){
-        //this.totalZombies = new LinkedList<NormalZombie>[5];
+        this.createZombieArrayList();
         this.lawnGraphic = new LawnSquare[Constants.LAWN_ROWS][Constants.LAWN_COLUMN];
         this.plantBoard = new PeaShooter[Constants.LAWN_ROWS][Constants.LAWN_COLUMN];
         this.setUpLawn(gamepane);
+        this.setUpZombieTimeline(gamepane);
+    }
+    private void createZombieArrayList(){
+        this.totalZombies = new ArrayList<LinkedList<NormalZombie>>(Constants.LAWN_ROWS);
+        for (int i = 0; i < Constants.LAWN_ROWS; i++){
+            this.totalZombies.add(i, new LinkedList<NormalZombie>());
+        }
     }
 
     private void setUpLawn(Pane gamepane) {
@@ -37,6 +50,12 @@ public class Lawn {
             }
         }
 
+    }
+    private void setUpZombieTimeline(Pane root){
+        KeyFrame kf = new KeyFrame(Duration.millis(2000), (ActionEvent e) -> this.generateZombies(root));
+        Timeline timeline = new Timeline(kf);
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
     }
 
 
@@ -79,11 +98,12 @@ public class Lawn {
         }
         return spotOpen;
     }
-    private void generateZombies(int Y, Pane root){
-        int randY = this.randomY();
-
+    private void generateZombies(Pane root){
+        int randY = this.randomYpixel();
+        NormalZombie newZombie = new NormalZombie(randY, root);
+        this.totalZombies.get(this.pixelToRow(randY)).add(newZombie);
     }
-    private int randomY(){
+    private int randomYpixel(){
         int randY = ((int)(Math.random() * 5) + 1) * Constants.LAWN_WIDTH;
         return randY;
     }
--- NormalZombie.java
+++ NormalZombie.java
@@ -1,8 +1,13 @@
 package indy;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 
 public class NormalZombie {
     private Rectangle zombieHitbox;
@@ -11,5 +16,16 @@ public class NormalZombie {
         this.zombieHitbox = new Rectangle(Constants.SCENE_WIDTH, Y, Constants.ZOMBIE_WIDTH, Constants.LAWN_WIDTH);
         this.zombieHitbox.setFill(Color.GRAY);
         root.getChildren().add(this.zombieHitbox);
+        this.setUpWalkingTimeline();
+    }
+    private void setUpWalkingTimeline(){
+        KeyFrame kf = new KeyFrame(Duration.millis(20), (ActionEvent e) -> this.walk());
+        Timeline timeline = new Timeline(kf);
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+    }
+    private void walk(){
+        this.zombieHitbox.setX(this.zombieHitbox.getX() - 2);
     }
 }
+
